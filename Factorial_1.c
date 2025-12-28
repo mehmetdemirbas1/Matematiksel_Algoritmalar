c77e175133371d6cf5cd5e52f7519ca43b70b3cd
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2025-12-28 14:24:39 +0300

    Recursive bir şekilde faktoriyel alan algoritma eklendi

---
+++ Factorial_1.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int Factorial(int fac)
+{
+	if (fac == 0 || fac == 1)
+		return 1;
+
+	return fac * Factorial(fac - 1);
+}
+
+int main()
+{
+	int number;
+	printf("Bir sayi giriniz:");
+	(void)scanf("%d", &number);
+
+	printf("%d! = %d", number, Factorial(number));
+}
