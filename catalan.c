d4fa5d491abbb760acada6fed7f6d4c2fd4263ff
Mehmet Demirbas <mehmet1demirbas@gmail.com>
2026-01-02 15:43:33 +0300

    catalan sayıları yazdıran c kodu

---
+++ catalan.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+long int factorial(int x)
+{
+    int i;
+    long int fac;
+    fac = x;
+    for (i = 1; i < x; i++)
+    {
+        fac = fac * (x - i);
+    }
+    return fac;
+}
+int main()
+{
+    long int f1, f2, f3;
+    int n;
+    float C;
+    scanf("%d", &n);
+    f1 = factorial(2 * n);
+    f2 = factorial(n + 1);
+    f3 = factorial(n);
+    C = f1 / (f2 * f3);
+    printf("%0.2f", C);
+    return 0;
+}
